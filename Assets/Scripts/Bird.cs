85ed830501c5da91fd6dcd37416c59b1165b9815
Matthew <Matthew.b.rodriguez@gmail.com>
2018-10-24 18:46:08 -0400

    Added bird animations

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -8,10 +8,12 @@ public class Bird : MonoBehaviour {
 
 	private bool isDead = false;
 	private Rigidbody2D rb2d;
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
 	}
 
 	// Update is called once per frame
@@ -20,9 +22,19 @@ public class Bird : MonoBehaviour {
 
 			// if player has left-clicked
 			if (Input.GetMouseButtonDown(0)) {
+
+				// set velocity to zero for consistent jump behavior
+				// add upforce on every tap
 				rb2d.velocity = Vector2.zero;
 				rb2d.AddForce(new Vector2(0, upForce));
+				anim.SetTrigger("Flap");
 			}
 		}
+	}
+
+	// Function from Unity's API
+	void OnCollisionEnter2D() {
+		isDead = true;
+		anim.SetTrigger("Die");
 	}
 }
